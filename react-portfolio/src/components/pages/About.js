64b7808a3256ac28e4839f829cd32bde1519bdb7
njenkins2727 <njenkins2727@gmail.com>
2022-09-26 13:47:52 +0800

    created react app, added navbar, currently editing components

---
+++ react-portfolio/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    
+    <div>
+      <h5>Hi my name is</h5>
+      <h1>Nathan Jenkins</h1>
+      <p>
+        I am a full stack developer who is going to continue to grow and thrive in this industry. 
+        <br></br>
+        Over the past 6 months I have learnt many new front/back end technologies. I have thoroughly
+        <br></br>
+        enjoyed learning in the fullstack web developer course and I'm excited to display my new skills.
+      </p>
+    </div>
+  );
+}
---
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio Page</h1>
+      
+      <div class="row row-cols-1 row-cols-sm-2 g-3">
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">E commerce art store</h5>
+        <img src='../../assets/e-commerce-home-page.jpeg' alt='E-commerce art store homepage'></img>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">JATE</h5>
+        <img src='../../assets/jate-screenshot.png' alt='jate text editor app'></img>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Note Taker</h5>
+        <img src='../../assets/note-taker.png' alt='note taker app'></img>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">README.md generator </h5>
+        <img src='../../assets/reademe-generator.png' alt='functioning README.md app'></img>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Schedule Planner</h5>
+        <img src='../../assets/schedule-planner-home.jpeg' alt='Schedule Planner app'></img>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="assets/images/bs-images/img-2x1.png" class="card-img-top" alt="card-grid-image"></img>
+      <div class="card-body">
+        <h5 class="card-title">Password Generator</h5>
+        <img src='../../assets/password-generator.png' alt='Password generator app'></img>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </div>
+  );
+}

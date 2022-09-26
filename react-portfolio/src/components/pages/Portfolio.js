f9748425c5a078c45d1a329da53ea48956d024e7
njenkins2727 <njenkins2727@gmail.com>
2022-09-26 17:12:36 +0800

    changed and added images to portfolio and icons now have links that are functional

--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -12,20 +12,20 @@ export default function Portfolio() {
         <div class="card-body">
           <h5 class="card-title">E commerce art store</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <img src={require('../../assets/icons/github-icon.png')} alt='github icon'  height='50px' ></img>        
-          <img src={require('../../assets/icons/linkedin.png')} alt='linkedin icon'  height='50px' ></img> 
+          <a href='https://github.com/timmobrien/e-commerce-art-store'> <img src={require('../../assets/icons/github-icon.png')} alt='github icon'  height='50px' ></img> </a>   
+          <a href='https://e-commerce-art-application.herokuapp.com/'> <img src={require('../../assets/icons/deployed-icon.jpeg')} alt='deployed icon'  height='50px' ></img> </a>
         </div>
       </div>
     </div>
 
     <div class="col">
       <div class="card">
-        <img src={require('../../assets/jate-screenshot.png')} alt='note taker app homepage' class="card-img-top" ></img> 
+        <img src={require('../../assets/jate-screenshot.png')} alt='jate text editor homepage' class="card-img-top" ></img> 
           <div class="card-body">
           <h5 class="card-title">JATE</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <img src={require('../../assets/icons/github-icon.png')} alt='github icon'  height='50px' ></img>        
-          <img src={require('../../assets/icons/linkedin.png')} alt='linkedin icon'  height='50px' ></img> 
+          <a href='https://github.com/njenkins2727/pwa-text-editor'> <img src={require('../../assets/icons/github-icon.png')} alt='github icon'  height='50px' ></img>  </a>      
+          <a href='https://jate-text-editor-app.herokuapp.com/'><img src={require('../../assets/icons/deployed-icon.jpeg')} alt='deployed icon'  height='50px' ></img> </a>
         </div>
       </div>
     </div>
@@ -36,44 +36,43 @@ export default function Portfolio() {
         <div class="card-body">
           <h5 class="card-title">Note Taker</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-          <img src={require('../../assets/icons/github-icon.png')} alt='github icon'  height='50px' ></img>        
-          <img src={require('../../assets/icons/linkedin.png')} alt='linkedin icon'  height='50px' ></img> 
+          <a href='https://github.com/njenkins2727/note-taker'> <img src={require('../../assets/icons/github-icon.png')} alt='github icon'  height='50px' ></img>  </a>       
+          <a href='https://your-note-taking.herokuapp.com/'> <img src={require('../../assets/icons/deployed-icon.jpeg')} alt='deployed icon'  height='50px' ></img> </a>
         </div>
       </div>
     </div>
 
     <div class="col">
       <div class="card">
-        <img src={require('../../assets/reademe-generator.png')} alt='note taker app homepage' class="card-img-top" ></img> 
+        <img src={require('../../assets/reademe-generator.png')} alt='functional read.me generator' class="card-img-top" ></img> 
           <div class="card-body">
             <h5 class="card-title">README.md generator </h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <img src={require('../../assets/icons/github-icon.png')} alt='github icon'  height='50px' ></img>        
-            <img src={require('../../assets/icons/linkedin.png')} alt='linkedin icon'  height='50px' ></img> 
+            <a href='https://github.com/njenkins2727/readme-generator'> <img src={require('../../assets/icons/github-icon.png')} alt='github icon'  height='50px' ></img>  </a>      
           </div>
        </div>
     </div>
 
     <div class="col">
       <div class="card">
-        <img src={require('../../assets/schedule-planner-home.jpeg')} alt='note taker app homepage' class="card-img-top" ></img> 
+        <img src={require('../../assets/work-schedule.png')} alt='scheduler app homepage' class="card-img-top" ></img> 
         <div class="card-body">
           <h5 class="card-title">Schedule Planner</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <img src={require('../../assets/icons/github-icon.png')} alt='github icon'  height='50px' ></img>        
-          <img src={require('../../assets/icons/linkedin.png')} alt='linkedin icon'  height='50px' ></img> 
+          <a href='https://github.com/njenkins2727/scheduler-planner'><img src={require('../../assets/icons/github-icon.png')} alt='github icon'  height='50px' ></img> </a>
+          <a href='https://njenkins2727.github.io/scheduler-planner/'><img src={require('../../assets/icons/deployed-icon.jpeg')} alt='deployed icon'  height='50px' ></img>  </a>
         </div>
       </div>
     </div>
 
     <div class="col">
       <div class="card">
-        <img src={require('../../assets/password-generator.png')} alt='note taker app homepage' class="card-img-top" ></img> 
+        <img src={require('../../assets/password-generator.png')} alt='password generator app homepage' class="card-img-top" ></img> 
         <div class="card-body">
           <h5 class="card-title">Password Generator</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <img src={require('../../assets/icons/github-icon.png')} alt='github icon'  height='50px' ></img>        
-          <img src={require('../../assets/icons/linkedin.png')} alt='linkedin icon'  height='50px' ></img> 
+          <a href='https://github.com/njenkins2727/random-password-generator'><img src={require('../../assets/icons/github-icon.png')} alt='github icon'  height='50px' ></img> </a>
+          <a href='https://njenkins2727.github.io/random-password-generator/'><img src={require('../../assets/icons/deployed-icon.jpeg')} alt='deployed icon'  height='50px' ></img> </a>
         </div>
       </div>
     </div>

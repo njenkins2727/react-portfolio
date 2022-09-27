c26b59c0075390352bc44e772d5cc82787f7f192
njenkins2727 <njenkins2727@gmail.com>
2022-09-27 18:31:12 +0800

    testing profile photo in about, work in progress

--- react-portfolio/src/pages/About/About.js
+++ react-portfolio/src/pages/About/About.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './About.css'
 
 export default function About() {
   return (
     
     <div class="text-center">
       <h5>Hi my name is</h5>
-      {/* <img src={require('../../assets/profile-photo.jpg')} alt='profile photo'  class="card-img-top"></img>  */}
+      <img src={require('../../assets/profile-photo.jpg')} alt='profile photo'  class="card-img-top profile-photo"></img> 
       <h1>Nathan Jenkins</h1>
       <p>
         I am a full stack developer who is going to continue to grow and thrive in this industry. 

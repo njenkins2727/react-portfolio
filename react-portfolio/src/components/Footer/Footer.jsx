56428c3c8160ac0dc7df678c8780336266fac3d6
njenkins2727 <njenkins2727@gmail.com>
2022-09-29 16:26:13 +0800

    edited footer

--- react-portfolio/src/components/Footer/Footer.jsx
+++ react-portfolio/src/components/Footer/Footer.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import githubIcon from "../../assets/icons/github-icon.png"
 import linkedinIcon from "../../assets/icons/linkedin.png"
 import stackOverflowIcon from "../../assets/icons/stackOverflow.png"
+import './Footer.css'
 
 function Footer(){
     return (
         <footer class="footer">
             <h5>Find me: </h5>
-            <img src={githubIcon} alt="github icon" />
-            <img src={linkedinIcon} alt="linkedin icon" />
-            <img src={stackOverflowIcon} alt="stackOverflow icon" />
-            <h5>Or my email - </h5>
-            <p>njenkins2727@gmail.com</p>
+            <a href="https://github.com/njenkins2727"> <img src={githubIcon} alt="github icon" class="socials" /> </a>
+            <a href="https://www.linkedin.com/in/nathan-jenkins-17798b236/"> <img src={linkedinIcon} alt="linkedin icon" class="socials"/> </a>
+            <a href="https://stackoverflow.com/users/18874031/njenkins2727"> <img src={stackOverflowIcon} alt="stackOverflow icon" class="socials"/> </a>
+            <h5>njenkins2727@gmail.com</h5>
         </footer>
           
     );
